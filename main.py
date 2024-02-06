f41c297e2ea0bc52e103be2712cea4b982f263e4
Luke-Trivette <157540127+Luke-Trivette@users.noreply.github.com>
2024-02-06 14:49:48 -0600

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def sum_of_products(list1, list2):
         return "Error: Lists must be the same length." 
     result = sum(x * y for x, y in zip(list1, list2))
     return result
-list1 = list(map(int, input()))
-list2 = list(map(int, input()))
+list1 = list(map(int,float, input()))
+list2 = list(map(int,float, input()))
 output = sum_of_products(list1, list2)
 print("Output =", output)
