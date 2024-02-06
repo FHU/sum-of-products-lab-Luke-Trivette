9798052d13c87a1cc2ddd986b827a4db180c9acc
Luke-Trivette <157540127+Luke-Trivette@users.noreply.github.com>
2024-02-06 12:44:03 -0600

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 def sum_of_products(list1, list2):
     if len(list1) != len(list2):
         return "Error: Lists must be the same length." 
+    x = 0
     result = sum(x * y for x, y in zip(list1, list2))
     return result
 list1 = list(map(int, input("Enter the first series of integers: ")))
 list2 = list(map(int, input("Enter the second series of integers: ")))
 output = sum_of_products(list1, list2)
 print("Output =", output)
+#oops
