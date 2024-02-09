6ec1a78de7963166fe7fc758bb478ddca6a4c593
Luke-Trivette <157540127+Luke-Trivette@users.noreply.github.com>
2024-02-09 09:46:17 -0600

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-def sum_of_products(list1, list2):
-    if len(list1) != len(list2):
-        return "Error: Lists must be the same length." 
-    result = sum(x * y for x, y in zip(list1, list2))
+def sum_of_products(series1, series2):
+    if len(series1) != len(series2):
+        return "Error: Lists must be the same length."
+    result = sum(a * b for a, b in zip(series1, series2))
+
     return result
-list1 = list(map(int, input().split()))
-list2 = list(map(int, input().split()))
-output = sum_of_products(list1, list2)
-print("Output =", output)
-#test
+
+if __name__ == "__main__":
+    try:
+        series1 = list(map(int, input().split()))
+        series2 = list(map(int, input().split()))
+        result = sum_of_products(series1, series2)
+        print("Sum of Products:", result)
+    except ValueError:
+        print("Error: Please enter valid integers.")
